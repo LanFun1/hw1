b4e44e7259ad767f848aa1277bdc88bc8121c5e1
LanFun1 <66485374+LanFun1@users.noreply.github.com>
2020-07-02 15:52:28 +0300

    sd
    s

--- Assets/Scripts/Signal.cs
+++ Assets/Scripts/Signal.cs
@@ -9,22 +9,18 @@ public class Signal : MonoBehaviour
     [SerializeField] private UnityEvent _thiefExit;
 
     private bool _courutineOpener = true;
-    private bool _corutineChanger = true;
+    private bool _volumeChanger = true;
     private float _volume = 0.3f;
+
     private void Update()
     {
-        if(_courutineOpener == true && _corutineChanger == true)
+        if(_courutineOpener == true)
         {
-            StopCoroutine(VolumeControllerMinus());
-            StartCoroutine(VolumeControllerPlus());
-        }
-        if (_courutineOpener == true && _corutineChanger == false)
-        {
-            StopCoroutine(VolumeControllerPlus());
-            StartCoroutine(VolumeControllerMinus());
-        }            
+            StartCoroutine(VolumeController());
+        }           
         _courutineOpener = false;
     }
+
     private void OnTriggerEnter2D()
     {
         _thiefEnter.Invoke();
@@ -34,29 +30,28 @@ public class Signal : MonoBehaviour
     {
         _thiefExit.Invoke();
     }
-    private IEnumerator VolumeControllerPlus()
+
+    private IEnumerator VolumeController()
     {
         var waitforsec = new WaitForSeconds(0.2f);
-        if (_volume <= 5f)
+        if (_volume <= 5f && _volumeChanger == true)
         {
             AudioListener.volume = _volume;
             _volume += 0.3f;
         }
         else
-            _corutineChanger = false;
-        yield return waitforsec;
-        _courutineOpener = true;
-    }
-    private IEnumerator VolumeControllerMinus()
-    {
-        var waitforsec = new WaitForSeconds(0.2f);
-        if (_volume >= 0.4f)
+        {
+            _volumeChanger = false;
+        }
+        if (_volume >= 0.4f && _volumeChanger == false)
         {
             AudioListener.volume = _volume;
             _volume -= 0.3f;
         }
         else
-            _corutineChanger = true;
+        {
+            _volumeChanger = true;
+        }
         yield return waitforsec;
         _courutineOpener = true;
     }
